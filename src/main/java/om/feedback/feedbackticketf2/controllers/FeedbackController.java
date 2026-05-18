5fafc12cebeef423a8cc4cdcabdc02cefc507977
RimaAhmed1 <rimaaltarshi@gmaile.com>
2026-05-18 12:26:46 +0400

    Creating basic classes for the feedback project

--- src/main/java/om/feedback/feedbackticketf2/controllers/FeedbackController.java
+++ src/main/java/om/feedback/feedbackticketf2/controllers/FeedbackController.java
@@ -1,23 +1,7 @@
 package om.feedback.feedbackticketf2.controllers;
-
-import om.feedback.feedbackticketf2.models.Feedback;
-import om.feedback.feedbackticketf2.services.FeedbackService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
-
 @RestController
 @RequestMapping(path="/coffefeedbacks")
 public class FeedbackController {
-    @Autowired
-    private FeedbackService feedbackService;
-    // create feedback
-    @PostMapping
-    public ResponseEntity<Feedback> storeIncomingFeedback(
-            @RequestBody Feedback incomingFeedback) {
-        Feedback createdFeedback = feedbackService.createFeedback(incomingFeedback);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdFeedback);
-    }
+
 }
--- src/main/java/om/feedback/feedbackticketf2/services/FeedbackService.java
+++ src/main/java/om/feedback/feedbackticketf2/services/FeedbackService.java
@@ -1,25 +1,6 @@
 package om.feedback.feedbackticketf2.services;
-
-import om.feedback.feedbackticketf2.models.Feedback;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
-
 @Service
 public class FeedbackService {
-    private final HashMap<Long, Feedback> feedbackStore = new HashMap<>();
 
-    // generating uniq id
-    private final AtomicLong counter = new AtomicLong();
-
-    // creating a new feedback
-    public Feedback createFeedback(Feedback givenFeedback) {
-        Long generatedId = counter.incrementAndGet();
-        givenFeedback.setId(generatedId);
-        feedbackStore.put(generatedId, givenFeedback);
-        return givenFeedback;
-    }
 }
