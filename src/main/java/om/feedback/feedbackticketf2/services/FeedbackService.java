a51cd6a9803030c1a23bd736f525e5bd0abfb89b
RimaAhmed1 <rimaaltarshi@gmaile.com>
2026-05-18 12:32:52 +0400

    creating feedback endpoint and post a feedback

--- src/main/java/om/feedback/feedbackticketf2/services/FeedbackService.java
+++ src/main/java/om/feedback/feedbackticketf2/services/FeedbackService.java
@@ -1,6 +1,22 @@
 package om.feedback.feedbackticketf2.services;
+import om.feedback.feedbackticketf2.models.Feedback;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 @Service
 public class FeedbackService {
+    private final HashMap<Long, Feedback> feedbackStore = new HashMap<>();
 
+    // generating uniq id
+    private final AtomicLong counter = new AtomicLong();
+
+    // creating a new feedback
+    public Feedback createFeedback(Feedback givenFeedback) {
+        Long generatedId = counter.incrementAndGet();
+        givenFeedback.setId(generatedId);
+        feedbackStore.put(generatedId, givenFeedback);
+        return givenFeedback;
+    }
 }
