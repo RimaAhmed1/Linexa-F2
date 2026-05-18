11e1a34dfeb7c5fd9a59fefdf5c2934bce9fa011
RimaAhmed1 <rimaaltarshi@gmaile.com>
2026-05-18 14:18:03 +0400

    reformate the code

--- src/main/java/om/feedback/feedbackticketf2/services/FeedbackService.java
+++ src/main/java/om/feedback/feedbackticketf2/services/FeedbackService.java
@@ -1,4 +1,5 @@
 package om.feedback.feedbackticketf2.services;
+
 import om.feedback.feedbackticketf2.models.Feedback;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +9,6 @@ import java.util.concurrent.atomic.AtomicLong;
 @Service
 public class FeedbackService {
     private final HashMap<Long, Feedback> feedbackStore = new HashMap<>();
-
     // generating uniq id
     private final AtomicLong counter = new AtomicLong();
 
